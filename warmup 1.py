563ee432d5383c24c4dfe6f780d1d2563b299c0e
unknown <akhilkannemadugu45@gmail.com>
2023-10-19 03:45:48 -0700

    Functions

---
+++ warmup 1.py
@@ -0,0 +1,89 @@
+#FUNCTIONS
+"""
+def func_name_pr():
+    print("hello")
+xx= func_name_pr()
+print("function data:",xx)#none
+
+
+def func_name_pr():
+    return("hello")
+xx= func_name_pr()
+print("function data:",xx)#hello
+
+
+def func_name_pr():
+    return("python")
+xx= func_name_pr()
+print("function data:",xx)#python
+
+"""
+
+#EMPTY ARGUMENTS
+"""
+def my_fav_place():
+    return("kedarnath")
+xx= my_fav_place()
+print(xx)#kedarnath
+
+def i_learn():
+    return("polymorphism")
+xx= i_learn()
+print(xx)#polymorphism
+
+
+def i_am_playing():
+     return "tennis"
+
+print(i_am_playing())#tennis
+
+"""
+
+
+
+#POSITIONAL ARGUMENTS
+
+    
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
